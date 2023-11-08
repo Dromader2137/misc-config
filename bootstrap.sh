24fcf35998218739d9771c51ac83a2531ac696d4
Dromader2137 <wojciechowski.ignacy@gmail.com>
2023-11-08 20:40:47 +0100

    sdfsdfsd

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,21 +4,24 @@ mkdir .config
 mkdir .config/nushell
 
 # Git & Rust
-sudo pacman -S --needed git rustup
+sudo pacman -S --needed --noconfirm git rustup
 rustup default stable
 
 # AUR Helper
-sudo pacman -S --needed base-devel
+sudo pacman -S --needed --noconfirm base-devel
 git clone https://aur.archlinux.org/paru.git
 cd paru
 makepkg -si
 cd ..
 
 # Xorg
-paru -S xorg xorg-xinit picom-git
+paru -S --noconfirm xorg xorg-xinit xorg-xrandr picom-git
 
 # Tools
-paru -S neovim alacritty zip unzip dmenu npm librewolf-bin lazygit
+paru -S --noconfirm zip unzip dmenu npm librewolf-bin lazygit nvim-packer-git bluez bluez-utils
+systemctl enable --now bluetooth.service
+
+#WM
 git clone https://github.com/leftwm/leftwm.git
 cd leftwm
 cargo build --profile optimized
@@ -28,33 +31,40 @@ sudo ln -s "$(pwd)"/target/optimized/lefthk-worker /usr/bin/lefthk-worker
 sudo ln -s "$(pwd)"/target/optimized/leftwm-state /usr/bin/leftwm-state
 sudo ln -s "$(pwd)"/target/optimized/leftwm-check /usr/bin/leftwm-check
 sudo ln -s "$(pwd)"/target/optimized/leftwm-command /usr/bin/leftwm-command
-cd ../.config
-git clone https://github.com/Dromader2137/nvim-config nvim/
-git clone https://github.com/Dromader2137/alacritty-config alacritty/
-git clone https://github.com/Dromader2137/leftwm-config leftwm/
-git clone https://github.com/Dromader2137/nushell-config nushell/
 cd ..
+git clone https://github.com/Dromader2137/leftwm-config .config/leftwm/
 cp -r leftwm/themes .config/leftwm
 rm .config/leftwm/themes/README.md
 ln -s .config/leftwm/themes/dr-theme .config/leftwm/themes/current
-cargo install bat
-cargo install du-dust
-cargo install exa
-cargo install wiki-tui
-cargo install cargo-info
-cargo install starship
-cargo install bottom
-cargo install speedtest-rs
 
-# Shell
+#Audio
+paru -S --noconfirm pipewire pipewire-audio wireplumber pipewire-pulse
+systemctl enable --now pipewire-pulse.service
+
+#Text editors
+paru -S --noconfirm neovim nvim-packer-git
+git clone https://github.com/Dromader2137/nvim-config .config/nvim/
+paru -S --noconfirm vim
+paru -S --noconfirm nano
+paru -S --noconfirm helix
+
+#Terminal emulator
+paru -S --noconfirm alacritty
+git clone https://github.com/Dromader2137/alacritty-config alacritty/
+
+#Shell
+git clone https://github.com/Dromader2137/nushell-config nushell/
 cargo install nu
+cargo install starship
+.cargo/bin/starship preset bracketed-segments -o .config/starship.toml
 sudo cp .cargo/bin/nu /usr/bin/nu
 sudo sh -c 'echo "/usr/bin/nu" >> /etc/shells'
 chsh -s /usr/bin/nu
 
-# Final
-paru -S ttf-mononoki-nerd ttf-nerd-fonts-symbols ttf-nerd-fonts-symbols-common ttf-nerd-fonts-symbols-mono
-git clone https://github.com/Dromader2137/misc-config
+#Misc
+cargo install cargo-info
+cargo install bottom
 mv misc-config/.xinitrc .xinitrc
 
-startx
+# Font
+paru -S --noconfirm ttf-mononoki-nerd ttf-nerd-fonts-symbols ttf-nerd-fonts-symbols-common ttf-nerd-fonts-symbols-mono noto-fonts-emoji
