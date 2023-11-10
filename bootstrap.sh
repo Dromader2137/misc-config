2c9a3dbac9d476db5c71b3bc7e62a61a16cef50b
Dromader2137 <wojciechowski.ignacy@gmail.com>
2023-11-10 19:31:43 +0100

    sfgdfgsfg

--- bootstrap.sh
+++ bootstrap.sh
@@ -20,7 +20,7 @@ yay -S --noconfirm xorg xorg-xinit xorg-xrandr picom
 yay -S --noconfirm pipewire pipewire-audio wireplumber pipewire-pulse pipewire-jack
 
 # Tools
-yay -S --noconfirm zip unzip dmenu npm librewolf-bin lazygit nvim-packer-git bluez bluez-utils cmake
+yay -S --noconfirm zip unzip dmenu npm librewolf-bin lazygit nvim-packer-git bluez bluez-utils cmake polybar
 systemctl enable --now bluetooth.service
 
 #WM
